b18cd3fa341c695189f7dc84cf27729ad3912f10
knonomura <katsuhiko.nonomura@griddb.org>
2020-12-03 16:28:55 +0900

    add Dockerfile for java-client

---
+++ java-client/run-griddb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$GRIDDB_CLUSTER_NAME" ]; then
+    GRIDDB_CLUSTER_NAME='dockerGridDB'
+fi
+
+if [ -z "$NOTIFICATION_ADDRESS" ]; then
+    NOTIFICATION_ADDRESS=239.0.0.1
+fi
+
+if [ -z "$NOTIFICATION_PORT" ]; then
+    NOTIFICATION_PORT=31999
+fi
+
+if [ -z "$GRIDDB_USERNAME" ]; then
+    GRIDDB_USERNAME='admin'
+fi
+
+if [ -z "$GRIDDB_PASSWORD" ]; then
+    GRIDDB_PASSWORD='admin'
+fi
+
+echo "parameter : $NOTIFICATION_ADDRESS $NOTIFICATION_PORT $GRIDDB_CLUSTER_NAME $GRIDDB_USERNAME $GRIDDB_PASSWORD"
+export CLASSPATH=${CLASSPATH}:/root/./gridstore.jar
+javac gsSample/Sample1.java
+java gsSample/Sample1 $NOTIFICATION_ADDRESS $NOTIFICATION_PORT $GRIDDB_CLUSTER_NAME $GRIDDB_USERNAME $GRIDDB_PASSWORD
+
